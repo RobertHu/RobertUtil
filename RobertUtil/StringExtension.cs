ce453a4799dae6a7413420d6e5a8c1181dff6be8
Robert <654693729@qq.com>
2013-04-12 10:28:28 +0800

    edit stringextension

--- RobertUtil/StringExtension.cs
+++ RobertUtil/StringExtension.cs
@@ -7,6 +7,11 @@ namespace CommonUtil
 {
     public static class StringExtension
     {
+        /// <summary>
+        /// string is null or empty
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns></returns>
         public static bool IsNullOrEmpty(this string source)
         {
             return string.IsNullOrEmpty(source);
