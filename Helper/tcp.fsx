1d410f270a51d737818eb28daffce5e46292124d
Robert <654693729@qq.com>
2013-04-12 11:19:41 +0800

    doing

---
+++ Helper/tcp.fsx
@@ -0,0 +1,3 @@
+﻿open System.Net
+open System.Net.Sockets
+let client = new TcpClient("do3.iexchange.bz",8000);
--- JavaTraderTest/Program.cs
+++ JavaTraderTest/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.ServiceModel;
 using iExchange.Common;
 using System.Diagnostics;
+using System.Net.Sockets;
 
 namespace JavaTraderTest
 {
@@ -12,19 +13,14 @@ namespace JavaTraderTest
     {
         static void Main(string[] args)
         {
-            string url = "net.tcp://ws0210:9001/AsyncSslServer/Service/CommandCollectService";
-            EndpointAddress address = new EndpointAddress(url);
-            NetTcpBinding binding = new NetTcpBinding(SecurityMode.None);
-            binding.MaxBufferPoolSize = binding.MaxReceivedMessageSize = binding.MaxBufferSize = 16 * 1024 * 1024;
-            //binding.SendTimeout = TimeSpan.FromSeconds(3);
-            binding.OpenTimeout = TimeSpan.FromSeconds(1);
-            ChannelFactory<ICommandCollectService> factory = new ChannelFactory<ICommandCollectService>(binding, address);
-            ICommandCollectService commandCollectService = factory.CreateChannel();
-            Token token = new Token();
-            QuoteCommand quoteCommand = new QuoteCommand();
-            commandCollectService.AddCommand(token, quoteCommand);
-            Console.WriteLine("ok");
-            Console.Read();
+            try
+            {
+                TcpClient client = new TcpClient("do3.iexchange.bz", 8000);
+            }
+            catch (Exception ex)
+            {
+
+            }
         }
     }
 }
